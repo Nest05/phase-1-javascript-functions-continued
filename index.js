0ca6ae9d5680f55ed90930e9f5827a384fac8c67
Nestor Masinde <nestormasinde@gmail.com>
2023-12-12 17:10:54 +0300

    Functions Continued

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+
+    return `This Saturday, I want to ${activity}!`;
+
+}
+activity('bathe my dog');
+
+function mondayWork(activity1 = 'go to the office'){
+    return `This Monday, I will ${activity1}.`;
+
+}
+activity1('work from home');
+
+
+function wrapAdjective(star){
+
+    return function(feel = 'special'){
+        
+        return `You are ${star}${feel}${star}!`;
+    }
+}
+console.log(wrapAdjective('*')('Awesome')('*'));
